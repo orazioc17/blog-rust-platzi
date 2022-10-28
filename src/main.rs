872100dd2f7c26682f53edd1120995731c489e90
Orazio Cappadonna <oraziocappadonna1999@gmail.com>
2022-10-28 16:21:41 -0400

    Haciendo update con diesel

--- src/main.rs
+++ src/main.rs
@@ -32,19 +32,9 @@ fn main() {
         .expect("La insertada fallo");
     */
     // Select * from posts
-/*     let posts_result = posts
-        .load::<Post>(&mut conn)
-        .expect("Error excecuting query");*/
-    // En este caso no es un select all, sino que se limita el resultado
-    println!("\nQuery con limit 1");
-    let posts_result = posts.limit(1) // con Este limit se limita la query a 1
-        .load::<Post>(&mut conn)
-        .expect("Error excecuting query");
-
-    // Imprimiendo el titulo de cada post recibido
-    for post in posts_result {
-        println!("{}", post.title);
-    }
+    /*     let posts_result = posts
+    .load::<Post>(&mut conn)
+    .expect("Error excecuting query");*/
 
     // Query sin limites
     println!("\nQuery sin limites");
@@ -56,7 +46,19 @@ fn main() {
         println!("{:?}", post);
     }
 
-    // SELECT title, body FROM posts limit 1 
+    /*
+    // En este caso no es un select all, sino que se limita el resultado
+    println!("\nQuery con limit 1");
+    let posts_result = posts.limit(1) // con Este limit se limita la query a 1
+        .load::<Post>(&mut conn)
+        .expect("Error excecuting query");
+
+    // Imprimiendo el titulo de cada post recibido
+    for post in posts_result {
+        println!("{}", post.title);
+    }
+
+    // SELECT title, body FROM posts limit 1
     println!("\nQuery con columnas especificas");
     let posts_result = posts.limit(1).select((title, body))
         .load::<PostSimplificado>(&mut conn) // Se usa el modelo PostSimplificado que se creo para esta query
@@ -86,5 +88,23 @@ fn main() {
     for post in posts_result {
         println!("{:?}", post);
     }
+    */
+    // Cambiando valores con un update
+    /*
+    let post_update = diesel::update(posts.filter(id.eq(2)))
+        // Para cambiar varios campos a la vez, se pasan los valores dentro de un set (valor1.eq(...), valor2.eq(...))
+        .set((slug.eq("segundo-post"), title.eq("Mi segundo blogpost")))
+        .get_result::<Post>(&mut conn)
+        .expect("Error en el update");
+    */
+    println!("\nQuery sin limites");
+    let posts_result = posts // Sin limit es como hacer un SELECT * FROM table
+        .order(id)
+        .load::<Post>(&mut conn)
+        .expect("Error excecuting query");
+    
+    for post in posts_result {
+        println!("{:?}", post);
+    } 
     
 }
