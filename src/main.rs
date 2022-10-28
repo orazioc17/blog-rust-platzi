799c78f51d280c593d6f0a608c8ed562c0ed07a0
Orazio Cappadonna <oraziocappadonna1999@gmail.com>
2022-10-28 11:36:13 -0400

    Creando los registros en la db

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,44 @@
 #[macro_use]
 extern crate diesel;
 
-pub mod schema;
 pub mod models;
+pub mod schema;
 
 use dotenvy::dotenv;
 use std::env;
 
-use diesel::prelude::*;
-use diesel::pg::PgConnection; // Encargado de realizar la conexion a postgres como tal
+use diesel::pg::PgConnection;
+use diesel::prelude::*; // Encargado de realizar la conexion a postgres como tal
 
 fn main() {
-
     dotenv().ok();
 
     let db_url = env::var("DATABASE_URL").expect("db url no encontrada");
 
     let mut conn = PgConnection::establish(&db_url).expect("we didn't connect to the database");
-    use self::models::Post;
+    use self::models::{NewPost, Post};
+    use self::schema::posts;
     use self::schema::posts::dsl::*;
 
+    let new_post = NewPost {
+        title: "Mi tercer blogpost",
+        body: "lorem ipsum",
+        slug: "tercer-post",
+    };
+    /*
+    let post: Post = diesel::insert_into(posts::table)
+        .values(&new_post)
+        .get_result(&mut conn)
+        .expect("La insertada fallo");
+    */
     // Select * from posts
-    let posts_result = posts.load::<Post>(&mut conn).expect("Error excecuting query");
+//    let posts_result = posts
+//        .load::<Post>(&mut conn)
+//        .expect("Error excecuting query");
+    // En este caso no es un select all, sino que se limita el resultado
+    let posts_result = posts.limit(1) // con Este limit se limita la query a 1
+        .load::<Post>(&mut conn)
+        .expect("Error excecuting query");
 
     for post in posts_result {
         println!("{}", post.title);
--- src/models.rs
+++ src/models.rs
@@ -5,3 +5,15 @@ pub struct Post {
     pub slug: String,
     pub body: String,
 }
+
+// Los datos deben tener cierto formato antes de entrar en la base de datos, lo cual se hace a continuacion
+use super::schema::posts;
+
+#[derive(Insertable)]
+#[table_name="posts"]
+pub struct NewPost<'a> {
+    // No hay que introducir el ID porque este se autogenera y se va autoincrementando
+    pub title: &'a str,
+    pub body: &'a str,
+    pub slug: &'a str,
+}
