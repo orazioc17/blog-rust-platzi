72cb70bbb74c26a035bceae310bbb4ef3852a270
Orazio Cappadonna <oraziocappadonna1999@gmail.com>
2022-10-29 09:06:00 -0400

    Creando endpoint POST para los posts

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ pub mod schema;
 use actix_web::web::Data;
 use dotenvy::dotenv;
 use std::env;
-use std::fmt::format;
 
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
@@ -18,19 +17,35 @@ use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 pub type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
-use self::models::Post;
+use self::models::{NewPost, Post, NewPostHandler};
 use self::schema::posts;
 use self::schema::posts::dsl::*;
 
 #[get("/")]
-async fn index(pool: web::Data<DbPool>) -> impl Responder {
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hola vale")
+}
+
+#[get("/posts")]
+async fn get_posts(pool: web::Data<DbPool>) -> impl Responder {
     let mut conn = pool.get().expect("Problema al traer la base de datos");
 
     // block lo que hace es que en el thread que estamos se bloquea para que nadie mas pueda acceder
     match web::block(move || posts.load::<Post>(&mut conn)).await {
-        Ok(data) => {
-            HttpResponse::Ok().body(format!("{:?}", data))
-        }
+        Ok(data) => HttpResponse::Ok().body(format!("{:?}", data)),
+        Err(err) => HttpResponse::Ok().body("Hubo un error al recibir data"),
+    }
+}
+
+#[post("/posts")]
+async fn create_posts(pool: web::Data<DbPool>, item: web::Json<NewPostHandler>) -> impl Responder {
+    let conn = pool.get().expect("Problema al traer la base de datos");
+
+    // block lo que hace es que en el thread que estamos se bloquea para que nadie mas pueda acceder
+    match web::block(move || { Post::create_post(conn, &item) })
+    .await
+    {
+        Ok(data) => HttpResponse::Ok().body(format!("{:?}", data)),
         Err(err) => HttpResponse::Ok().body("Hubo un error al recibir data"),
     }
 }
@@ -51,6 +66,8 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .service(index)
+            .service(get_posts)
+            .service(create_posts)
             .app_data(web::Data::new(pool.clone())) // Esta es la forma de utilizarlo con app_data, pues data esta deprecado y era data(pool.clone())
     })
     .bind(("127.0.0.1", 8080))?
--- src/models.rs
+++ src/models.rs
@@ -1,5 +1,8 @@
+use diesel::{PgConnection, r2d2::{PooledConnection, ConnectionManager}};
+use serde::{Deserialize, Serialize};
+
 // Debug, como su nombre lo indica, sirve para debuguear (imprimir con {:?})
-#[derive(Queryable, Debug)]
+#[derive(Queryable, Debug, Serialize, Deserialize)]
 pub struct Post {
     pub id: i32,
     pub title: String,
@@ -9,9 +12,17 @@ pub struct Post {
 
 // Los datos deben tener cierto formato antes de entrar en la base de datos, lo cual se hace a continuacion
 use super::schema::posts;
+use diesel::prelude::*;
+
+#[derive(Queryable, Debug, Serialize, Deserialize)]
+pub struct PostSimplificado {
+    // Se creo este modelo para hacer una query que obtenga unicamente esos campos
+    pub title: String,
+    pub slug: String,
+}
 
 #[derive(Insertable)]
-#[table_name="posts"]
+#[table_name = "posts"]
 pub struct NewPost<'a> {
     // No hay que introducir el ID porque este se autogenera y se va autoincrementando
     pub title: &'a str,
@@ -19,8 +30,31 @@ pub struct NewPost<'a> {
     pub slug: &'a str,
 }
 
-#[derive(Queryable, Debug)]
-pub struct PostSimplificado{ // Se creo este modelo para hacer una query que obtenga unicamente esos campos
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct NewPostHandler {
     pub title: String,
-    pub slug: String,
+    pub body: String,
+}
+
+impl Post {
+    pub fn slugify(title: String) -> String {
+        return title.replace(" ", "-").to_lowercase();
+    }
+
+    pub fn create_post<'a>(
+        mut conn: PooledConnection<ConnectionManager<PgConnection>>,
+        post: &NewPostHandler,
+    ) -> Result<Post, diesel::result::Error> {
+        let slug = Post::slugify(post.title.clone());
+
+        let new_post = NewPost {
+            title: &post.title,
+            slug: &slug,
+            body: &post.body,
+        };
+
+        diesel::insert_into(posts::table)
+            .values(new_post)
+            .get_result::<Post>(&mut conn)
+    }
 }
