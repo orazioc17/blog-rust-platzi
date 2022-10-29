b7ce3bcc7ec05842700529312cb98e5f5cf04d33
Orazio Cappadonna <oraziocappadonna1999@gmail.com>
2022-10-28 23:05:30 -0400

    Hola mundo actix y preparacion para usarlo con diesel

--- src/main.rs
+++ src/main.rs
@@ -1,124 +1,40 @@
 #[macro_use]
 extern crate diesel;
 
-pub mod models;
 pub mod schema;
+pub mod models;
 
 use dotenvy::dotenv;
 use std::env;
 
+use diesel::prelude::*;
 use diesel::pg::PgConnection;
-use diesel::prelude::*; // Encargado de realizar la conexion a postgres como tal
 
-fn main() {
+use diesel::r2d2::{self, ConnectionManager, Pool};
+
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello_world() -> impl Responder {
+    HttpResponse::Ok().body("<h1>Hello World<h1><h2>Hola<h2>")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+
     dotenv().ok();
+    let db_url = env::var("DATABASE_URL").expect("db url variable no encontrada");
 
-    let db_url = env::var("DATABASE_URL").expect("db url no encontrada");
+    // Con esto creamos un manager de conexiones, no una sola conexion sino todas las que se vayan a necesitar
+    let connection = ConnectionManager::<PgConnection>::new(db_url);
 
-    let mut conn = PgConnection::establish(&db_url).expect("we didn't connect to the database");
-    use self::models::{NewPost, Post, PostSimplificado};
-    use self::schema::posts;
-    use self::schema::posts::dsl::*;
+    // El Pool que creamos sirve para tener acceso a la base de datos desde las views
+    let pool = Pool::builder().build(connection).expect("No se pudo construir la Pool");
 
-    let new_post = NewPost {
-        title: "Mi tercer blogpost",
-        body: "lorem ipsum",
-        slug: "tercer-post",
-    };
-    /*
-    let post: Post = diesel::insert_into(posts::table)
-        .values(&new_post)
-        .get_result(&mut conn)
-        .expect("La insertada fallo");
-    */
-    // Select * from posts
-    /*     let posts_result = posts
-    .load::<Post>(&mut conn)
-    .expect("Error excecuting query");*/
+    HttpServer::new(move || {
+        App::new()
+            .service(hello_world)
+            .app_data(web::Data::new(pool.clone())) // Esta es la forma de utilizarlo con app_data, pues data esta deprecado y era data(pool.clone())
+    }).bind(("127.0.0.1", 8080))?.run().await
 
-    // Query sin limites
-    println!("\nQuery sin limites");
-    let posts_result = posts // Sin limit es como hacer un SELECT * FROM table
-        .load::<Post>(&mut conn)
-        .expect("Error excecuting query");
-
-    for post in posts_result {
-        println!("{:?}", post);
-    }
-
-    /*
-    // En este caso no es un select all, sino que se limita el resultado
-    println!("\nQuery con limit 1");
-    let posts_result = posts.limit(1) // con Este limit se limita la query a 1
-        .load::<Post>(&mut conn)
-        .expect("Error excecuting query");
-
-    // Imprimiendo el titulo de cada post recibido
-    for post in posts_result {
-        println!("{}", post.title);
-    }
-
-    // SELECT title, body FROM posts limit 1
-    println!("\nQuery con columnas especificas");
-    let posts_result = posts.limit(1).select((title, body))
-        .load::<PostSimplificado>(&mut conn) // Se usa el modelo PostSimplificado que se creo para esta query
-        .expect("Error excecuting query");
-
-    for post in posts_result {
-        println!("{:?}", post);
-    }
-
-    // SELECT * FROM posts order by id limit 1
-    println!("\nQuery con el order by limit 1");
-    let posts_result = posts.order(id.desc()).limit(1) // id.desc para traerlos de mayor a menor
-        .load::<Post>(&mut conn) // Se usa el modelo PostSimplificado que se creo para esta query
-        .expect("Error excecuting query");
-
-    for post in posts_result {
-        println!("{:?}", post);
-    }
-
-    // SELECT * FROM posts WHERE
-    println!("\nQuery con un where especifico");
-    let posts_result = posts
-        .filter(id.eq(2)) // id.eq es para traer ese id especifico (equals)
-        .load::<Post>(&mut conn) // Se usa el modelo PostSimplificado que se creo para esta query
-        .expect("Error excecuting query");
-
-    for post in posts_result {
-        println!("{:?}", post);
-    }
-    */
-    // Cambiando valores con un update
-    /*
-    let post_update = diesel::update(posts.filter(id.eq(2)))
-        // Para cambiar varios campos a la vez, se pasan los valores dentro de un set (valor1.eq(...), valor2.eq(...))
-        .set((slug.eq("segundo-post"), title.eq("Mi segundo blogpost")))
-        .get_result::<Post>(&mut conn)
-        .expect("Error en el update");
-    */
-    println!("\nQuery sin limites");
-    let posts_result = posts // Sin limit es como hacer un SELECT * FROM table
-        .order(id)
-        .load::<Post>(&mut conn)
-        .expect("Error excecuting query");
-
-    for post in posts_result {
-        println!("{:?}", post);
-    }
-
-    // Borrando un registro
-    diesel::delete(posts.filter(slug.like("%-post%")))
-        .execute(&mut conn)
-        .expect("Ha fallado la eliminacion del tercer post");
-
-    println!("\nQuery sin limites");
-    let posts_result = posts // Sin limit es como hacer un SELECT * FROM table
-        .order(id)
-        .load::<Post>(&mut conn)
-        .expect("Error excecuting query");
-    
-    for post in posts_result {
-        println!("{:?}", post);
-    }
 }
